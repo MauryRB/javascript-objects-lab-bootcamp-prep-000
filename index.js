491411332fe12d59b1cb5ebd6b50653f180c5a43
Amaury Burgos <amauryb.1204@gmail.com>
2020-11-16 18:43:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
+  Object.assign({}, object)
   delete object[key]
-  return Object.assign({}, object)
+  return object
 }
